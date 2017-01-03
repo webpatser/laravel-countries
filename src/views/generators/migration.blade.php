bd0a45aa8d72ac021f4b6de39b6423bda6365c0f
Keith O'Faoláin <vool@users.noreply.github.com>
2017-01-03 01:00:21 +0000

    Set id field as increments

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -12,7 +12,7 @@ class SetupCountriesTable extends Migration {
 		// Creates the users table
 		Schema::create(\Config::get('countries.table_name'), function($table)
 		{		    
-		    $table->integer('id')->unsigned()->index();
+		    $table->increments('id');
 		    $table->string('capital', 255)->nullable();
 		    $table->string('citizenship', 255)->nullable();
 		    $table->string('country_code', 3)->default('');
@@ -30,7 +30,6 @@ class SetupCountriesTable extends Migration {
 		    $table->string('calling_code', 3)->nullable();
 		    $table->string('flag', 6)->nullable();
 		    
-		    $table->primary('id');
 		});
 	}
 
