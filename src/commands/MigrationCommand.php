a7dc5c16b08a63445ba791f22c5b490939b37052
Mateus Felipe C. C. Pinto <mateusfccp@gmail.com>
2018-01-03 16:28:19 -0200

    Update MigrationCommand.php

--- src/commands/MigrationCommand.php
+++ src/commands/MigrationCommand.php
@@ -53,9 +53,7 @@ class MigrationCommand extends Command {
             {
                 $this->line('');
 
-                if (version_compare(app()->version(), '5.5.0', '<')) {
-                    $this->call('optimize', []);
-                }
+                $this->call('optimize', []);
 
                 $this->line('');
 
