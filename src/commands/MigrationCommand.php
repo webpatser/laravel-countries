860e1ab5fa5e19fe362c2b19f4150961374f71b3
Christoph Kempen <christoph@downsized.nl>
2018-05-16 08:22:32 +0200

    command handle/fire for compatibility for older laravel versions

--- src/commands/MigrationCommand.php
+++ src/commands/MigrationCommand.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Webpatser\Countries;
 
@@ -31,13 +31,13 @@ class MigrationCommand extends Command {
         $app = app();
         $app['view']->addNamespace('countries',substr(__DIR__,0,-8).'views');
     }
-
+    
     /**
      * Execute the console command.
      *
      * @return void
      */
-    public function fire()
+    public function handle()
     {
         $this->line('');
         $this->info('The migration process will create a migration file and a seeder for the countries list');
@@ -60,7 +60,7 @@ class MigrationCommand extends Command {
                 $this->info( "Migration successfully created!" );
             }
             else{
-                $this->error( 
+                $this->error(
                     "Coudn't create migration.\n Check the write permissions".
                     " within the app/database/migrations directory."
                 );
@@ -97,7 +97,7 @@ class MigrationCommand extends Command {
 
         $seconds = 0;
 
-        foreach ($migrationFiles as $migrationFile => $outputFile) {            
+        foreach ($migrationFiles as $migrationFile => $outputFile) {
             if (sizeof(glob($migrationFile)) == 0) {
                 $migrationFile = str_replace('*', date('Y_m_d_His', strtotime('+' . $seconds . ' seconds')), $migrationFile);
 
@@ -131,5 +131,12 @@ class MigrationCommand extends Command {
 
         return true;
     }
-
+    
+    /**
+     * BC for older laravel versions
+     */
+    public function fire()
+    {
+        $this->handle();
+    }
 }
